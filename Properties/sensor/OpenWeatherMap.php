c24d55d06d37b0f527554f3f80b3f047bd5d6d60
TheBradleySanders <>
2021-06-30 15:20:38 -0500

    Update OpenWeatherMap.php

--- Properties/sensor/OpenWeatherMap.php
+++ Properties/sensor/OpenWeatherMap.php
@@ -1,14 +1,14 @@
 <?php
-namespace Modules\MQTT\Properties\sensor;
+namespace Modules\OpenWeatherMap\Properties\sensor;
 
 use App\PropertyTypes\sensor\sensor;
 use App\Helpers\SettingManager;
 
 /**
- * Class MQTT
+ * Class OpenWeatherMap
  * @package App\PropertyTypes\sensor
  */
-class MQTT extends sensor
+class OpenWeatherMap extends sensor
 {
     public $supportedAttributes = ["connected"];
 
@@ -16,29 +16,13 @@ class MQTT extends sensor
         $this->meta = $meta;
         $this->features = $this->getFeatures($this);
 
-        $this->mqttConnected = false;
+        $username = SettingManager::get('apikey', 'openweathermap')['value'];
 
-        // MQTT Host Settings
-        $host = SettingManager::get('host', 'mqtt')['value'];
-        $port = SettingManager::get('port', 'mqtt')['value'];
-        $username = SettingManager::get('username', 'mqtt')['value'];
-        $password = SettingManager::get('password', 'mqtt')['value'];
-        $will = "";
-        $clientID = "SimpleHome".rand(1,100);
-
-        // Create MQTT Connection
-        $this->MQTT = new \Modules\MQTT\phpMQTT($host, $port, $clientID);
-        if($this->MQTT){
-            if($this->MQTT->connect(true, $will, $username, $password)){
-                $this->mqttConnected = true;
-            }
-        }
-        $this->setAttributes('connected', (int)$this->mqttConnected);
+        $this->setAttributes('connected', 0);
     }
 
     // API (GET): http://localhost/api/v2/device/(hostname)/state/(value)
     public function state($value){
-        // This is where you control the light
 
         // This is how you notify Simple Home of the state change
         $this->setState('state', $value);
